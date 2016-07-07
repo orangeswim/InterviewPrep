2eed54c26c364bafc124a7699f906b6c4568d155
rob <robwstine@outlook.com>
2016-07-07 19:45:53 -0400

    first commit

---
+++ Tests/interviewPrep/GeneralTests.java
@@ -0,0 +1,18 @@
+package interviewPrep;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import interviewPrep.GeneralQuestions;
+
+public class GeneralTests {
+
+	@Test
+	public void test() {
+
+		GeneralQuestions helloThere = new GeneralQuestions();
+		
+	}
+
+}
---
+++ src/interviewPrep/GeneralQuestions.java
@@ -0,0 +1,19 @@
+package interviewPrep;
+
+
+
+public class GeneralQuestions {
+	
+	public GeneralQuestions(){};
+	
+	
+/*	Find the most frequent integer in an array */
+
+	public int frequentInt(int [] arr)
+	{
+		
+		
+		return 0;
+	}
+
+}
