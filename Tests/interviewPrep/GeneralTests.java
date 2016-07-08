29faa110b8062c74248bf2acb2f98c642f24d59e
rob <robwstine@outlook.com>
2016-07-07 20:34:07 -0400

    First part of integer summing to 10 problem

--- Tests/interviewPrep/GeneralTests.java
+++ Tests/interviewPrep/GeneralTests.java
@@ -10,12 +10,14 @@ public class GeneralTests {
 
 	@Test
 	public void test() {
-
 		
 		GeneralQuestions Test = new GeneralQuestions();
-		int [] testIntegerFrequent = {0,1,2,3,1,1,4,2};
 		
+		int [] testIntegerFrequent = {0,1,2,3,1,1,4,2};		
 		assertTrue(Test.frequentInt(testIntegerFrequent) == 1);
+		
+		int [] testAmount = {10,0,0,10,5,5};		
+		assertTrue(Test.pairsSorted(testAmount) == 3);
 		
 	}
 
--- src/interviewPrep/GeneralQuestions.java
+++ src/interviewPrep/GeneralQuestions.java
@@ -1,6 +1,8 @@
 package interviewPrep;
 
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class GeneralQuestions {
 	
@@ -34,4 +36,29 @@ public class GeneralQuestions {
 		return arr[maxIndex];
 	}
 
+	public int pairsSorted(int [] arr)
+	{
+		ArrayList<Integer> numList = new ArrayList<Integer>();
+		for(int i = 0; i < arr.length;i++)
+		{
+			numList.add(arr[i]);
+		}
+		Collections.sort(numList);
+		int pair = 0;
+		
+		//Minor enhancement can be made by only testing 1/2 the numbers.
+		for(int i : numList)
+		{
+			if(numList.contains(10-i))
+			{
+				pair++;
+			}
+		}
+				
+		return pair/2;
+	}
+			
+			
+			
+	
 }
